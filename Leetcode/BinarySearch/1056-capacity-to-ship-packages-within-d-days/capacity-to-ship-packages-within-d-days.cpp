e4e16b4eb447aab93d2a6e06e9088297cccfa076
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-01-19 09:40:40 -0500

    Time: 41 ms (90.40%) | Memory: 33.5 MB (10.38%) - LeetSync

--- Leetcode/BinarySearch/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ Leetcode/BinarySearch/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -26,16 +26,16 @@ public:
         }
         int l = ma;
         int r = m;
-        int ans = 0;
+        // int ans = 0;
         while(l<=r){
             int mid = (l+r)/2;
             if(func(weights, mid) <= days){
                 r = mid -1;
-                ans = mid;
+                // ans = mid;
             }else{
                 l = mid+1;
             }
         }
-        return ans;
+        return l;
     }
 };
