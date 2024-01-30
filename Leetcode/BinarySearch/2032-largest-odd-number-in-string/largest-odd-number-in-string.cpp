8b45b9f48732efaf794e5e2591b68f03c42785da
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-01-30 10:41:26 -0500

    Time: 32 ms (29.92%) | Memory: 16.3 MB (12.02%) - LeetSync

---
+++ Leetcode/BinarySearch/2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int index = -1;
+        for(int i=num.length(); i>=0; i--){
+            int temp = (int)num[i];
+            if(temp%2 == 1){
+                return num.substr(0, i+1);
+            }
+        }
+        return "";
+    }
+};
