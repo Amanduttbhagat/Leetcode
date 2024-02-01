ad262766ac170dcf7971983fc6a30a9702a4915d
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-02-01 09:01:50 -0500

    Time: 3 ms (97.13%) | Memory: 8.4 MB (10.81%) - LeetSync

--- Leetcode/Strings/242-valid-anagram/valid-anagram.cpp
+++ Leetcode/Strings/242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // unordered_map<string, int> s_map;
+        // unordered_map<string, int> t_map;
+        // for(int i = 0;i<s.length();i++){
+        //     s_map[s.substr(i,1)] = s_map[s.substr(i,1)] + 1;
+        //     t_map[t.substr(i,1)] = t_map[t.substr(i,1)] + 1;
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     if(s_map[s.substr(i,1)] != t_map[s.substr(i,1)]){
+        //         return false;
+        //     }
+        // }
+        // return true;
         if(s.length() != t.length()){
             return false;
         }
-        unordered_map<string, int> s_map;
-        unordered_map<string, int> t_map;
-        for(int i = 0;i<s.length();i++){
-            s_map[s.substr(i,1)] = s_map[s.substr(i,1)] + 1;
-            t_map[t.substr(i,1)] = t_map[t.substr(i,1)] + 1;
+        int q[26];
+        for(char c: s){
+            q[c - 'a']++;
         }
-        for(int i=0;i<s.length();i++){
-            if(s_map[s.substr(i,1)] != t_map[s.substr(i,1)]){
+        for(char c: t){
+            q[c - 'a']--;
+        }
+        for(int i=0;i<size(q);i++){
+            if(q[i] != 0){
                 return false;
             }
         }
