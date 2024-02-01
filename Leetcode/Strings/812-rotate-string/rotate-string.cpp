7b221e4e5844183337d6058ab918b8df38c7595e
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-02-01 08:15:37 -0500

    Time: 5 ms (10.82%) | Memory: 8.5 MB (5.28%) - LeetSync

---
+++ Leetcode/Strings/812-rotate-string/rotate-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            string h = s.substr(0,i+1);
+            // cout<<h<<endl;
+            string q = s.substr(i+1);
+            // cout<<q<<endl;
+            q += h;
+            // cout<<ans<<endl;
+            if(q == goal){
+                return true;
+            }
+        }
+        return false;
+    }
+};
