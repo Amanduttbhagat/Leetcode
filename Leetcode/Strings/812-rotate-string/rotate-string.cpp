e58be75d090e958f942d6dfadc4f14f2588ed858
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-02-01 08:27:09 -0500

    Time: 0 ms (100.00%) | Memory: 7.4 MB (15.02%) - LeetSync

--- Leetcode/Strings/812-rotate-string/rotate-string.cpp
+++ Leetcode/Strings/812-rotate-string/rotate-string.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.length() != goal.length()){
-            return false;
-        }
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            string h = s.substr(0,i+1);
-            // cout<<h<<endl;
-            string q = s.substr(i+1);
-            // cout<<q<<endl;
-            q += h;
-            // cout<<ans<<endl;
-            if(q == goal){
-                return true;
-            }
+        // if(s.length() != goal.length()){
+        //     return false;
+        // }
+        // int n = s.length();
+        // for(int i=0;i<n;i++){
+        //     string h = s.substr(0,i+1);
+        //     // cout<<h<<endl;
+        //     string q = s.substr(i+1);
+        //     // cout<<q<<endl;
+        //     q += h;
+        //     // cout<<ans<<endl;
+        //     if(q == goal){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        string ans = s+s;
+        if((s.length()==goal.length())&&ans.find(goal) < s.length()){
+            return true;
         }
         return false;
     }
