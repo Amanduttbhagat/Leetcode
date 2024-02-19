6215bac53568a1f910b2962fd9d5c15c98f524e9
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-02-19 13:18:17 -0500

    Time: 14 ms (97.80%) | Memory: 26.5 MB (40.93%) - LeetSync

--- Leetcode/StackQueue/503-next-greater-element-ii/next-greater-element-ii.cpp
+++ Leetcode/StackQueue/503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> res;
-        for(int i=0;i<nums.size();i++){
-            int ele = nums[i];
-            bool a = false;
-            for(int j=0;j<nums.size();j++){
-                if(nums[i] < nums[(i+j+1)%nums.size()]){
-                    res.push_back(nums[(i+j+1)%nums.size()]);
-                    a = true;
-                    break;
-                }
+    vector<int> nextGreaterElements(vector<int>& nums){
+        vector<int> res(nums.size(), -1);
+        std::stack<int> st;
+        int n = nums.size();
+        for(int i=0;i<n*2;i++){
+            while(!st.empty() && nums[st.top()] < nums[i%n]){
+                res[st.top()] = nums[i%n];
+                st.pop();
             }
-            if(!a){
-                res.push_back(-1);
-            }
+            if(i<n) st.push(i);
         }
         return res;
     }
+// O(n2) time and O(n) space complexity
+    // vector<int> nextGreaterElements(vector<int>& nums) {
+    //     vector<int> res;
+    //     for(int i=0;i<nums.size();i++){
+    //         int ele = nums[i];
+    //         bool a = false;
+    //         for(int j=0;j<nums.size();j++){
+    //             if(nums[i] < nums[(i+j+1)%nums.size()]){
+    //                 res.push_back(nums[(i+j+1)%nums.size()]);
+    //                 a = true;
+    //                 break;
+    //             }
+    //         }
+    //         if(!a){
+    //             res.push_back(-1);
+    //         }
+    //     }
+    //     return res;
+    // }
 };
