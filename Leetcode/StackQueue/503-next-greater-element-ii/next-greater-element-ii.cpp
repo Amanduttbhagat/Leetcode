f687bc775d0489f08aab1e24e71486e8e6f7c241
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-02-23 18:05:47 -0500

    Time: 24 ms (58.55%) | Memory: 26.7 MB (30.71%) - LeetSync

--- Leetcode/StackQueue/503-next-greater-element-ii/next-greater-element-ii.cpp
+++ Leetcode/StackQueue/503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,18 +1,55 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums){
-        vector<int> res(nums.size(), -1);
-        std::stack<int> st;
         int n = nums.size();
-        for(int i=0;i<n*2;i++){
-            while(!st.empty() && nums[st.top()] < nums[i%n]){
-                res[st.top()] = nums[i%n];
+        stack<int> st;
+        vector<int> res(n,-1);
+        for (int i = n * 2 - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums[i % n]) {
                 st.pop();
             }
-            if(i<n) st.push(i);
-        }
-        return res;
+            if (!st.empty()) {
+                res[i % n] = st.top();
+            }
+            st.push(nums[i % n]);
     }
+
+    return res;
+        // for(int i=n-1;i>=0;i--){
+        //     while(!st.empty() && st.top() < nums[i]){
+        //         st.pop();
+        //     }
+        //     if (!st.empty()) {
+        //         res[i] = st.top();
+        //     }
+        //     st.push(nums[i]);
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     if(st.top() == nums[i]){
+        //         break;
+        //     }
+        //     while(!st.empty() && st.top() < nums[i]){
+        //         st.pop();
+        //     }
+        //     if (!st.empty()) {
+        //         res[i] = st.top();
+        //     }
+        //     st.push(nums[i]);
+        // }
+        // return res;
+    }
+    //     vector<int> res(nums.size(), -1);
+    //     std::stack<int> st;
+    //     int n = nums.size();
+    //     for(int i=0;i<n*2;i++){
+    //         while(!st.empty() && nums[st.top()] < nums[i%n]){
+    //             res[st.top()] = nums[i%n];
+    //             st.pop();
+    //         }
+    //         if(i<n) st.push(i);
+    //     }
+    //     return res;
+    // }
 // O(n2) time and O(n) space complexity
     // vector<int> nextGreaterElements(vector<int>& nums) {
     //     vector<int> res;
