b362f90bddbd860a658511059329c1d3bbfb9b39
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-03-05 13:47:46 -0500

    Time: 130 ms (15.13%) | Memory: 75.6 MB (7.18%) - LeetSync

--- Leetcode/StackQueue/217-contains-duplicate/contains-duplicate.cpp
+++ Leetcode/StackQueue/217-contains-duplicate/contains-duplicate.cpp
@@ -2,12 +2,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]){
-                return true;
-            }
+        set<int> s;
+        
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
         }
-        return false;
+
+        if(s.size() == nums.size()){
+            return false;
+        }else{
+            return true;
+        }
+        // std::sort(nums.begin(), nums.end());
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 };
