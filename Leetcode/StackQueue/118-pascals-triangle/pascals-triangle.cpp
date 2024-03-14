51e599e3c058d6dd43ef467caf2f52c93a5cca1f
Amanduttbhagat <71687328+Amanduttbhagat@users.noreply.github.com>
2024-03-14 15:12:30 -0400

    Time: 0 ms (100.00%) | Memory: 8.1 MB (17.11%) - LeetSync

---
+++ Leetcode/StackQueue/118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<int> dir = {1};
+        vector<vector<int>> res = {{1}}; 
+        if(numRows == 1){
+            return res;
+        }else{
+            int u = 1;
+            int l = 1;
+            for(int i=0;i<numRows-1;i++){
+                vector<int> inner;
+                inner.push_back(1);
+                for(int j=0;j<l-1;j++){
+                    inner.push_back(dir[j]+dir[j+1]);
+                }
+                inner.push_back(1);
+                res.push_back(inner);
+                dir = inner;
+                l = inner.size();
+            }
+        }
+        return res;
+    }
+};
